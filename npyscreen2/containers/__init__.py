c243e7e5bb6bf3a2ae5ac9738188d386047194ec
Pablo Barton <pablo.barton@gmail.com>
2014-11-08 00:37:51 -0500

    creating the Indexable class for clean, memory-efficient iteration over contained widgets in Containers

--- npyscreen2/containers/__init__.py
+++ npyscreen2/containers/__init__.py
@@ -7,3 +7,29 @@ from .gridcontainer import GridContainer
 
 import logging
 log = logging.getLogger('npyscreen2.containers')
+
+from itertools import islice
+
+
+class Indexable(object):
+    """
+    Recipe for a generator iterator which supports indexing and slicing.
+
+    Provides concise means for operations on contained Widgets for use in
+    Containers.
+
+    Aside from convenience, should offer performance benefits to Containers with
+    large numbers of Widgets.
+    """
+    def __init__(self, it):
+        self.it = iter(it)
+
+    def __iter__(self):
+        for elt in self.it:
+            yield elt
+
+    def __getitem__(self, index):
+        try:
+            return next(islice(self.it, index, index + 1))
+        except TypeError:
+            return list(islice(self.it, index.start, index.stop, index.step))
