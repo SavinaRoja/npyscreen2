63b75c276e051a7fddd1336771ec97115d582c12
Paul Barton <pablo.barton@gmail.com>
2014-11-07 19:28:21 -0500

    Container instantiation no longer has a critical recursion error

--- npyscreen2/containers/__init__.py
+++ npyscreen2/containers/__init__.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
-__all__ = ['Container']
+__all__ = ['Container', 'GridContainer']
 
 from .container import Container
+from .gridcontainer import GridContainer
 
 import logging
 log = logging.getLogger('npyscreen2.containers')
